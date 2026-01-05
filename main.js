e91073659e2538fe34fe5efa4af2c1c182107696
Nikita Alekseev <snowniksa@gmail.com>
2026-01-05 23:42:07 +0300

    added switching completed

--- main.js
+++ main.js
@@ -16,9 +16,12 @@ addBtn.addEventListener('click', function(e){
     }
 
     const newDiv = document.createElement("div")
-
     taskContainer.appendChild(newDiv);
-    
+      
+    const completedInput = document.createElement("input")
+    completedInput.type = 'checkbox';
+    newDiv.appendChild(completedInput);
+
     const newSpan = document.createElement("span")
     newSpan.textContent = textInput;
     newDiv.appendChild(newSpan);
@@ -26,10 +29,21 @@ addBtn.addEventListener('click', function(e){
     const deleteBtn = document.createElement("button")
     deleteBtn.textContent = "Удалить";
     newDiv.appendChild(deleteBtn);
-
+       
     deleteBtn.addEventListener('click', function(){
         newDiv.remove();
     })
 
     taskInput.value = "";
+    
+    completedInput.addEventListener("change", function() {
+      if(this.checked) {
+      newDiv.children[1].classList.add('completed')
+      } else {
+    newDiv.children[1].classList.remove('completed')
+      }
+    })
+   
+       
+
 })
